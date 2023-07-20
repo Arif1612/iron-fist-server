3063afa62c2a7368ded18ca92e65fc10c14656e6
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2023-07-20 11:48:38 +0600

    popular classes data collected from mongodb and shown to the localhost /classes server

--- iron-fist-server/index.js
+++ iron-fist-server/index.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -11,8 +11,7 @@ app.use(express.json());
 // connections
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://arif:<password>@iron-fist.ryngjnb.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@iron-fist.ryngjnb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,6 +24,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // try ar vitorei sob api related code lekte hobe mongodb connection ar jonno
+
+    /* ---------------------
+        homepage related route
+        -------------------------*/
+      
+    //   all the collections 
+      
+    await client.connect();
+    const classesCollection = client
+      .db("iron-fist")
+      .collection("popular-classes");
+
+    //   popular-classes
+    //  collection ar maddome datagula classesCollection ar vitore age thekei nia asselam r .get /classes ar maddome amra data gula amadar localhost a show krtese 
+      
+      app.get('/classes',async(req, res) => {
+        const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -34,7 +54,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
