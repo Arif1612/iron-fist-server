b6c13cf17b2a316e26348d89bf455113db0b741f
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2023-08-03 18:46:53 +0600

    check user admin or not

--- iron-fist-server/index.js
+++ iron-fist-server/index.js
@@ -103,6 +103,18 @@ async function run() {
       res.send(result);
     });
 
+    // security check
+    app.get("/users/admin/:email",verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({admin:false})
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -127,7 +139,7 @@ async function run() {
      *************** */
 
     // get verifyJWT agula pore add krse
-    app.get("/student-carts",verifyJWT, async (req, res) => {
+    app.get("/student-carts", verifyJWT, async (req, res) => {
       const email = req.query.email;
       // console.log(email);
       if (!email) {
@@ -136,11 +148,11 @@ async function run() {
 
       const decodedEmail = req.query.email;
       if (email !== decodedEmail) {
-        return res.status(403).send({error:true,message:'forbidden access'})
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
       }
 
-
-      
       const query = { email: email };
       const result = await studentCartCollection.find(query).toArray();
       // console.log(result);
