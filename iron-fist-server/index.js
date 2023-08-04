9bf963f9dad098fd47674911cc9816fa95d7354a
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2023-08-04 10:01:17 +0600

    admin dashboard manage users works perfectly

--- iron-fist-server/index.js
+++ iron-fist-server/index.js
@@ -73,10 +73,24 @@ async function run() {
     app.post("/jwt", (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
+        expiresIn: "6h",
       });
       res.send({ token });
     });
+
+    // warning: use verifyJWT before using verify Admin
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
 
     //  collection ar maddome datagula classesCollection ar vitore age thekei nia asselam r .get /classes ar maddome amra data gula amadar localhost a show krtese
 
@@ -84,7 +98,7 @@ async function run() {
      * users related API
      **************** */
 
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyJWT,verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -104,11 +118,11 @@ async function run() {
     });
 
     // security check
-    app.get("/users/admin/:email",verifyJWT, async (req, res) => {
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
 
       if (req.decoded.email !== email) {
-        res.send({admin:false})
+        res.send({ admin: false });
       }
       const query = { email: email };
       const user = await usersCollection.findOne(query);
@@ -139,7 +153,7 @@ async function run() {
      *************** */
 
     // get verifyJWT agula pore add krse
-    app.get("/student-carts", verifyJWT, async (req, res) => {
+    app.get("/student-carts", async (req, res) => {
       const email = req.query.email;
       // console.log(email);
       if (!email) {
